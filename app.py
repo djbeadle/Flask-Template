1e24389d59b2355949a760938feb8e8de94bfbae
Daniel Beadle <djbeadle@gmail.com>
2019-04-10 16:29:28 -0400

    Rename from myapp -> app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-myapp = Flask(__name__)
+app = Flask(__name__)
 
-@myapp.route("/")
+@app.route("/")
 def hello():
   return "This is Flask running on Azure App Service for Linux"
