93cb910f247fc169a542bcbc083a7c29afcd8cfa
Daniel Beadle <djbeadle@gmail.com>
2019-04-11 13:55:43 -0400

    Add templates and static folders

--- app/landing/routes.py
+++ app/landing/routes.py
@@ -1,5 +1,16 @@
+from flask import render_template
 from app.landing import landing_bp
 
 @landing_bp.route('/', methods=['GET'])
 def home():
-    return "This is the landing blueprint!"
+    text = 'This is the landing route!'
+    return render_template('landing.html', content=text)
+
+@landing_bp.route('/info', methods=['GET'])
+def info():
+    text = 'This is the info route! Here is a kitten:'
+    return render_template('info.html', content=text)
+
+@landing_bp.route('/info', methods=['PUT'])
+def update_info():
+    return 'You have made a put request!'
