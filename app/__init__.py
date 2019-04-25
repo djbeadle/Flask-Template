f109cd2b529980b76898c6d23e6bea99c04cf93e
Daniel Beadle <djbeadle@users.noreply.github.com>
2019-04-25 14:14:39 -0400

    Update __init__.py to support multiple configs

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, Blueprint
-from config import config
+from config import config, DevelopmentConfig, ProductionConfig
 
 def create_app(config_name):
     """Application factory that returns a fully formed instance of the app
@@ -12,8 +12,13 @@ def create_app(config_name):
     app = Flask(__name__, static_url_path="/static")
     print('this is the config name: {}'.format(config_name))
     
+    # Select the desired config object from FLASK_ENV environment variable
     app.config.from_object(config[config_name])
-    config[config_name].init_app(app)
+    try:
+      config[config_name].init_app(app)
+    except:
+      print('An error occurred initalizing the app. Be sure to set the environment \
+        variables FLASK_ENV=(development|production) and FLASK_APP=application.py')
 
     from app.landing import landing_bp
     app.register_blueprint(landing_bp)
