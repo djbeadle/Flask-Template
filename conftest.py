35803d6f7f0c930c7d646751e50f0f1c575b984d
Daniel Beadle <djbeadle@users.noreply.github.com>
2019-05-28 13:03:03 -0400

    Create conftest.py

---
+++ conftest.py
@@ -0,0 +1,3 @@
+# This file exists to ensure pytest adds the root directory to the python path.
+# Otherwise python scripts in /tests/ will be unable to import any modules
+# https://stackoverflow.com/a/49033954/9310118
