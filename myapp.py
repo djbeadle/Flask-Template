ef2f30fac261afbfbcfb332bb2cfa8535e9700b6
Daniel Beadle <djbeadle@users.noreply.github.com>
2019-08-26 21:59:50 -0400

    Imported the wrong environment variable! 😬

--- myapp.py
+++ myapp.py
@@ -7,4 +7,4 @@ from app import create_app
 import os
 
 print('Firing things up')
-app = create_app(os.getenv('FLASK_CONFIG') or 'production')
+app = create_app(os.getenv('FLASK_ENV') or 'production')
